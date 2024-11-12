b9bbe05f4a3b9198429c5a70e3b2c36706e608a2
Diego Cardoso <diegocar448@hotmail.com>
2024-11-12 16:08:36 -0300

    1

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,14 @@
+import { Heading, Text, Container } from "@chakra-ui/react"
+
+export default function index(){
+    return (
+        <Container>
+            <Heading my="30px">
+                Chakra UI Components                   
+            </Heading>
+            <Text>
+                Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+            </Text>
+        </Container>
+    );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+import { HStack } from "@chakra-ui/react"
+
+export default function Home() {
+  return (
+    <HStack>
+      <Button>Click me 222</Button>
+      <Button>Click me</Button>
+    </HStack>
+  );
+}
